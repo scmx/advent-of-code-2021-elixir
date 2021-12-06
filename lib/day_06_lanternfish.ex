43f029d38771e2999921bdabce952a14136c562c
Albert Arvidsson <albert.arvidsson@gmail.com>
2021-12-06 08:10:19 +0100

    Add Day06Lanternfish part 2

--- lib/day_06_lanternfish.ex
+++ lib/day_06_lanternfish.ex
@@ -1,42 +1,52 @@
 defmodule Adventofcode.Day06Lanternfish do
   use Adventofcode
 
-  alias __MODULE__.{Parser, Part1, State}
+  alias __MODULE__.{Parser, State}
 
   def part_1(input) do
     input
     |> Parser.parse()
     |> State.new()
-    |> Part1.solve()
-    |> State.total()
+    |> State.solve(80)
+  end
+
+  def part_2(input) do
+    input
+    |> Parser.parse()
+    |> State.new()
+    |> State.solve(256)
   end
 
   defmodule State do
     @enforce_keys []
-    defstruct days: 0, fishes: [], total: 0
+    defstruct days: 0, total: 0, fishes: %{}
 
-    def new(fishes), do: %__MODULE__{fishes: fishes, total: length(fishes)}
+    def new(fishes), do: %__MODULE__{fishes: fishes} |> update_total
 
-    def total(%State{total: total}), do: total
-  end
-
-  defmodule Part1 do
-    def solve(state, cycles \\ 80)
-    def solve(%State{days: cycles} = state, cycles), do: state
+    def solve(state, cycles \\ 256)
+    def solve(%State{days: cycles} = state, cycles), do: state.total
 
     def solve(state, cycles) do
       fishes = cycle(state.fishes)
 
-      %{state | days: state.days + 1, fishes: fishes, total: length(fishes)}
+      %{state | days: state.days + 1, fishes: fishes}
+      |> update_total
       |> solve(cycles)
     end
 
-    defp cycle(fishes) when is_list(fishes) do
-      Enum.flat_map(fishes, &cycle/1)
+    defp cycle(fishes) do
+      {pregnancies, rest} = Map.pop(fishes, 0, 0)
+
+      rest
+      |> Enum.map(fn {timer, count} -> {timer - 1, count} end)
+      |> Map.new()
+      |> Map.update(6, pregnancies, &(&1 + pregnancies))
+      |> Map.update(8, pregnancies, &(&1 + pregnancies))
     end
 
-    defp cycle(0), do: [6, 8]
-    defp cycle(n), do: [n - 1]
+    defp update_total(%State{} = state) do
+      %{state | total: state.fishes |> Map.values() |> Enum.sum()}
+    end
   end
 
   defmodule Parser do
@@ -45,6 +55,7 @@ defmodule Adventofcode.Day06Lanternfish do
       |> String.trim()
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
+      |> Enum.frequencies()
     end
   end
 end
--- test/day_06_lanternfish_test.exs
+++ test/day_06_lanternfish_test.exs
@@ -19,9 +19,19 @@ defmodule Adventofcode.Day06LanternfishTest do
     end
   end
 
+  describe "part_2/1" do
+    test "how many lanternfish after 256 days" do
+      assert 26_984_457_539 = @example |> part_2()
+    end
+
+    test_with_puzzle_input do
+      assert 1_705_008_653_296 = puzzle_input() |> part_2()
+    end
+  end
+
   describe "Parser.parse/1" do
     test "parses input" do
-      assert [3, 4, 3, 1, 2] = @example |> Parser.parse()
+      assert %{1 => 1, 2 => 1, 3 => 2, 4 => 1} = @example |> Parser.parse()
     end
   end
 end
