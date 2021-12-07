4780dbbb6f78549ded996fb08b0fee39a61e7ee4
Albert Arvidsson <albert.arvidsson@gmail.com>
2021-12-07 07:48:31 +0100

    Add Day07TheTreacheryOfWhales part 2

--- lib/day_07_the_treachery_of_whales.ex
+++ lib/day_07_the_treachery_of_whales.ex
@@ -1,12 +1,18 @@
 defmodule Adventofcode.Day07TheTreacheryOfWhales do
   use Adventofcode
 
-  alias __MODULE__.{Parser, Part1}
+  alias __MODULE__.{Parser, Part1, Part2}
 
   def part_1(input) do
     input
     |> Parser.parse()
     |> Part1.solve()
+  end
+
+  def part_2(input) do
+    input
+    |> Parser.parse()
+    |> Part2.solve()
   end
 
   defmodule Part1 do
@@ -19,6 +25,19 @@ defmodule Adventofcode.Day07TheTreacheryOfWhales do
 
     defp align(crab, crabs) do
       {crab, crabs |> Enum.map(&abs(&1 - crab)) |> Enum.sum()}
+    end
+  end
+
+  defmodule Part2 do
+    def solve(crabs) do
+      crabs
+      |> Enum.map(&align(&1, crabs))
+      |> Enum.min_by(&elem(&1, 1))
+      |> elem(1)
+    end
+
+    defp align(crab, crabs) do
+      {crab, crabs |> Enum.map(&Enum.sum(1..abs(&1 - crab - 1))) |> Enum.sum()}
     end
   end
 
--- test/day_07_the_treachery_of_whales_test.exs
+++ test/day_07_the_treachery_of_whales_test.exs
@@ -19,6 +19,16 @@ defmodule Adventofcode.Day07TheTreacheryOfWhalesTest do
     end
   end
 
+  describe "part_2/1" do
+    test "how much fuel needed to align" do
+      assert 168 = @example |> part_2()
+    end
+
+    test_with_puzzle_input do
+      assert 85_015_836 = puzzle_input() |> part_2()
+    end
+  end
+
   describe "Parser.parse/1" do
     test "parses input" do
       assert [16, 1, 2, 0, 4, 2, 7, 1, 2, 14] = @example |> Parser.parse()
