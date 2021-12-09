db523ea8246e8cd374021f31229c7bc44e2555e6
Albert Arvidsson <albert.arvidsson@gmail.com>
2021-12-09 07:08:41 +0100

    Add Day09SmokeBasin part 1

---
+++ lib/day_09_smoke_basin.ex
@@ -0,0 +1,59 @@
+defmodule Adventofcode.Day09SmokeBasin do
+  use Adventofcode
+
+  alias __MODULE__.{Coordinate, Parser, Part1}
+
+  def part_1(input) do
+    input
+    |> Parser.parse()
+    |> Part1.solve()
+  end
+
+  defmodule Coordinate do
+    @neighbours [
+      {0, 1},
+      {1, 0},
+      {0, -1},
+      {-1, 0}
+    ]
+    def neighbours({x, y}) do
+      Enum.map(@neighbours, fn {dx, dy} -> {x + dx, y + dy} end)
+    end
+  end
+
+  defmodule Part1 do
+    def solve(state) do
+      state
+      |> Enum.filter(&low_point?(&1, state))
+      |> Enum.map(fn {_, height} -> height + 1 end)
+      |> Enum.sum()
+    end
+
+    defp low_point?({{x, y}, height}, state) do
+      {x, y}
+      |> Coordinate.neighbours()
+      |> Enum.map(&Map.get(state, &1))
+      |> Enum.filter(& &1)
+      |> Enum.all?(&(height < &1))
+    end
+  end
+
+  defmodule Parser do
+    def parse(input) do
+      input
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.with_index()
+      |> Enum.flat_map(&parse_line/1)
+      |> Map.new()
+    end
+
+    defp parse_line({row, y}) do
+      row
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.with_index()
+      |> Enum.map(fn {cell, x} -> {{x, y}, cell} end)
+    end
+  end
+end
---
+++ test/day_09_smoke_basin_test.exs
@@ -0,0 +1,36 @@
+defmodule Adventofcode.Day09SmokeBasinTest do
+  use Adventofcode.FancyCase
+
+  import Adventofcode.Day09SmokeBasin
+
+  alias Adventofcode.Day09SmokeBasin.{Parser}
+
+  @example """
+  2199943210
+  3987894921
+  9856789892
+  8767896789
+  9899965678
+  """
+
+  describe "part_1/1" do
+    test "" do
+      assert 15 = @example |> part_1()
+    end
+
+    test_with_puzzle_input do
+      assert 575 = puzzle_input() |> part_1()
+    end
+  end
+
+  describe "Parser.parse/1" do
+    test "parses input" do
+      assert %{
+               {3, 3} => 7,
+               {4, 0} => 9,
+               {2, 1} => 8
+               # ...
+             } = @example |> Parser.parse()
+    end
+  end
+end
